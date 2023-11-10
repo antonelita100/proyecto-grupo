f4719a76075179a3e92d453bf353967629bbcbd6
lynchv-udesa <lynchv@udesa.edu.ar>
2023-11-10 12:06:37 -0300

    logo tmdb

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 
 /* https://api.themoviedb.org/3/movie/76341?api_key=a42737f60c529bfe7e9586db8cb132a1c */
 
-let url = https://api.themoviedb.org/3/movie/76341?api_key=a42737f60c529bfe7e9586db8cb132a1c
+let url = https://api.themoviedb.org/3/movie/76341?api_key=42737f60c529bfe7e9586db8cb132a1c
+
+
 let 
 
 
@@ -17,11 +19,11 @@ fetch(url)
     console.log(datos.url);
     let array = datos.provincias;
 
-    for ( let i = 0 ; i < array.length; i++) {
+   /* for ( let i = 0 ; i < array.length; i++) {
         console.log(array[i].nombre);
         listaProvincias.innerHTML += `<li><a href="./detalleProvincia.html?id_provincia=${array[i].id}">${array[i].nombre}</a></li>`
     }
-
+*/
     
     return datos;
 })
