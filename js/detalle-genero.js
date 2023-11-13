e602959c89fd778e2c648139a4b63e2f45c2ec1e
antonelita100 <aaxelrad@udesa.edu.ar>
2023-11-13 17:41:05 -0300

    js

--- js/detalle-genero.js
+++ js/detalle-genero.js
@@ -3,7 +3,7 @@ let qsObj = new URLSearchParams(qs);
 let idPelicula = qsObj.get("idPelicula");
 let apiKey = "42737f60c529bfe7e9586db8cb132a1c";
 
-let url = `https://api.themoviedb.org/3/discover/movie`;
+let url = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}`;
 let url2 = `https://api.themoviedb.org/3/discover/tv`;
 
 let contenido = document.querySelector(".titulo")
@@ -17,9 +17,8 @@ fetch(url)
     let array = datos.genres;
     console.log(array)
 
-    for (let i = 0; i < 5; i++) {
-      contenido.innerHTML += `<h2 class="titulo">${array[i].name}</h2>`;
-    }
+    contenido.innerHTML += `<h2 class="titulo">${array[0].name}</h2>`;
+    
 
     return datos;
   })
