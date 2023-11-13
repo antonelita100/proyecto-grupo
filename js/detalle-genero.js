c51153ba5b0aa3180a26d5df8d1a056956db91ef
antonelita100 <aaxelrad@udesa.edu.ar>
2023-11-13 17:30:42 -0300

    Creacion genero y detalle-genero

---
+++ js/detalle-genero.js
@@ -0,0 +1,30 @@
+let qs = location.search;
+let qsObj = new URLSearchParams(qs);
+let idPelicula = qsObj.get("idPelicula");
+let apiKey = "42737f60c529bfe7e9586db8cb132a1c";
+
+let url = `https://api.themoviedb.org/3/discover/movie`;
+let url2 = `https://api.themoviedb.org/3/discover/tv`;
+
+let contenido = document.querySelector(".titulo")
+
+
+fetch(url)
+  .then(function (r) {
+    return r.json();
+  })
+  .then(function (datos) {
+    let array = datos.genres;
+    console.log(array)
+
+    for (let i = 0; i < 5; i++) {
+      contenido.innerHTML += `<h2 class="titulo">${array[i].name}</h2>`;
+    }
+
+    return datos;
+  })
+
+  .catch(function (error) {
+    console.log(error);
+    return error;
+  });
---
+++ js/genero.js
@@ -0,0 +1,46 @@
+let apiKey = "42737f60c529bfe7e9586db8cb132a1c";
+let url = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}`
+let url2 = `https://api.themoviedb.org/3/genre/tv/list?api_key=${apiKey}`
+
+let contenido = document.querySelector(".navGenero");
+let contenido2 = document.querySelector("#nav");
+
+fetch(url)
+  .then(function (r) {
+    return r.json();
+  })
+  .then(function (datos) {
+    let array = datos.genres;
+    console.log(array)
+
+    for (let i = 0; i < 9; i++) {
+      contenido.innerHTML += `<li class="itemGenero"><a href="./detalle-genero.html" class="text">${array[i].name}</a></li>`;
+    }
+
+    return datos;
+  })
+
+  .catch(function (error) {
+    console.log(error);
+    return error;
+  });
+
+  fetch(url2)
+  .then(function (r) {
+    return r.json();
+  })
+  .then(function (datos) {
+    let array = datos.genres;
+    console.log(array)
+
+    for (let i = 0; i < 9; i++) {
+      contenido2.innerHTML += `<li class="itemGenero"><a href="./detalle-genero.html" class="text">${array[i].name}</a></li>`;
+    }
+
+    return datos;
+  })
+
+  .catch(function (error) {
+    console.log(error);
+    return error;
+  });
