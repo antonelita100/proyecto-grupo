287ce61c4876d23bd059b3d5346a49a0dfaee044
--global <--global>
2023-11-15 22:40:52 -0300

    iniciando ver recomendaciones de detalles js

--- js/detallePeli.js
+++ js/detallePeli.js
@@ -7,7 +7,6 @@ let APIkey = "42737f60c529bfe7e9586db8cb132a1c"
 let url = `https://api.themoviedb.org/3/movie/${idPelicula}?api_key=${APIkey}`
 
 
-
 let titulo = document.querySelector(".tituloserie-peli");
 let imagen = document.querySelector("#imgSerie");
 let descripcion = document.querySelector(".p-pelicula");
@@ -29,7 +28,7 @@ fetch(url)
    duracion.innerText = "Duracion: " + data.runtime +"mins"
    titulo.innerText = data.title;
    descripcion.innerText = data.overview;
-   anio.innerText = data.release_date
+   anio.innerText = "Estreno: " + data.release_date
    imagen.src = `https://image.tmdb.org/t/p/w500/${data.poster_path}`;
    genero.innerText = "Genero: " + data.genres[1].name
 
--- js/detalleSerie.js
+++ js/detalleSerie.js
@@ -4,38 +4,41 @@ let idSerie = qsObj.get("idSerie")
 
 let APIkey = "42737f60c529bfe7e9586db8cb132a1c"
 
-let url = `https://api.themoviedb.org/3/tv/${idSerie}?api_key=${APIkey}`
+let urlS = `https://api.themoviedb.org/3/tv/${idSerie}?api_key=${APIkey}`
+
 
 let titulo = document.querySelector(".tituloserie-peli");
-let imagen = document.querySelector(".divImage");
-let descripcion = document.querySelector("#p-serie");
+let imagen = document.querySelector("#imgSerie");
+let descripcion = document.querySelector(".p-serie");
 let rating = document.querySelector(".rating")
 let anio = document.querySelector(".anio")
-let duracion  = document.querySelector(".duracion")
 let genero = document.querySelector(".datos")
 let temporada = document.querySelector(".temporada")
 
-fetch(url)
-.then(function(res) {
+
+fetch(urlS) 
+.then(function(res){
     return res.json();
 })
 
-.then(function(data) {
+.then(function(data){
+    console.log(data)
+    rating.innerText = data.vote_average    ;
+    titulo.innerText = data.name;
+    descripcion.innerText = data.overview;
+    anio.innerText = "Estreno: " + data.first_air_date;
+    imagen.src = `https://image.tmdb.org/t/p/w500/${data.poster_path}`;
+    genero.innerText = "Genero: " + data.genres[1].name
+    temporada.innerText = "Temporadas: " + data.number_of_seasons;
+    
+    /*let generosTexto = "Género: ";
+    for (let i = 0; i < data.genres.length; i++) {
+        generosTexto += data.genres[i].name;
+        if (i < data.genres.length - 1) {
+            generosTexto += ', '; }} */
 
-   console.log(data);
-   rating.innerText = data.vote_count
-   duracion.innerText = "Duracion: " + data.runtime +"mins"
-   titulo.innerText = data.original_name;
-   descripcion.innerText = data.overview;
-   anio.innerText = data.first_air_date
-   temporada.innerText = data.number_of_seasons
-   imagen.src = `https://image.tmdb.org/t/p/w500/${data.poster_path}`;
-   genero.innerText = "Genero: " + data.genres[1].name
-
-   
 })
 
+.catch(function(error){
 
-.catch(function(error) {
-    return error
 })
